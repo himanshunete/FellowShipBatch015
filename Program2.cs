4d411964cdecffc66c703df18401d0e9bb678a2a
himanshunete <himanshuneteh@gmail.com>
2021-02-01 19:29:29 +0530

    [himanshunete]Modified Variable is used

---
+++ Program2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string aFriend = "Bill";
+            Console.WriteLine(aFriend);
+            Console.WriteLine($"Hello {aFriend}");
+
+
+        }
+    }
+}
